275508ba7390858b3a8ad8ab1193c2e025441b17
Misha <mike.afanasiuk.21@gmail.com>
2018-08-05 19:08:03 +0300

    code refactoring fixed bugs v 0.02

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,141 +1,89 @@
 'use strict';
 
-function componentToHex(c) {
-  let hex = c.toString(16);
-  return hex.length === 1 ? "0" + hex : hex;
-}
-
-function rgbToHex(r, g, b) {
-  return "#" + componentToHex(+r) + componentToHex(+g) + componentToHex(+b);
-}
-
 const folders = document.getElementById('folders-container');
-let lastSelected;
+let lastSelected = '';
 
-const checkFolderState = (state) => (
-  state === 'block'
-);
-
-const changeState = (state, element, triangleIconState) => {
-  element.parentNode.children[0].style.webkitTransform = `rotate(${triangleIconState}deg)`;
-  element.parentNode.children[0].style.transform = `rotate(${triangleIconState}deg)`;
-  while(element){
-    element.style.display = state;
+const changeState = (element) => {
+  element.previousElementSibling.children[0].classList.toggle('triangle-icon');
+  while (element) {
+    element.classList.toggle('show-sub-folder');
     element = element.nextElementSibling;
   }
 };
-
 const highlight = (element) => {
-  let colorString = element.style.color;
-  let colorArr = colorString.substring(colorString.indexOf('(') + 1, colorString.indexOf(')')).split(', ');
-  //let color = colorArr.length === 1 ? '' :  rgbToHex(...colorArr);
-  //let color = colorArr.length === 1 ? '' :  colorString;
-  let color = colorString.length === 0 ? '' :  colorString;
-  let checked = (color === 'red');
-  if (checked){
-    element.style.color = '';
-  }
-  else {
-    element.style.color = 'red';
-    lastSelected = element;
-  }
+  element.classList.toggle('active');
+  lastSelected = element;
+
 };
 
 const unHighlightAll = () => {
-  let elements = folders.getElementsByTagName('div');
+  let elements = folders.getElementsByTagName('span');
   elements = Array.prototype.slice.call(elements);
-  elements.forEach(item=> item.style.color = '')
-}
+  elements.forEach((item) => {
+    item.classList.toggle('active', false);
+  });
+};
 
-const nodesToParent = (target, parent) => {
-  let count = 0;
-  while (target !== parent){
-    count++;
-    target = target.parentNode;
-  }
-  return count;
-}
-
-const checkOrder = (e1, e2) => {
-  if(e1.parentNode === e2.parentNode){
-    let nodes  = e1.parentNode.childNodes;
-    nodes = Array.prototype.slice.call(nodes);
-    return (nodes.indexOf(e1) < nodes.indexOf(e2));
-  }
-  else {
-    let parent = document.getElementById('folders-container');
-    let c1 = nodesToParent(e1, parent);
-    let c2 = nodesToParent(e2, parent);
-    if(c1 < c2){
-      while (e1.parentNode !== e2.parentNode){
-        e2 = e2.parentNode;
-      }
-    } else {
-      while (e1.parentNode !== e2.parentNode){
-        e1 = e1.parentNode;
-      }
+const a = (firstElement, secondElement) => {
+  let flag = false;
+  firstElement.children[0].classList.toggle('active', true);
+  while (firstElement.querySelector('.show-sub-folder')) {
+    firstElement = firstElement.querySelector('.show-sub-folder');
+    firstElement.children[0].classList.toggle('active', true);
+    if (firstElement === secondElement) {
+      flag = true;
+      break;
     }
-    let nodes = e1.parentNode.childNodes;
-    nodes = Array.prototype.slice.call(nodes);
-    return (nodes.indexOf(e1) < nodes.indexOf(e2));
+  }
+  while (!flag) {
+    if (firstElement === secondElement) {
+      break;
+    }
+    if (firstElement.nextElementSibling) {
+      firstElement = firstElement.nextElementSibling;
+      firstElement.children[0].classList.toggle('active', true);
+    } else {
+      firstElement = firstElement.parentNode;
+    }
+  }
+};
+const multiplyHighlight2 = (secondElement, firstElement = lastSelected.parentElement) => {
+  if (firstElement.compareDocumentPosition(secondElement) === 4 || firstElement.compareDocumentPosition(secondElement) === 20) {
+    a(firstElement, secondElement);
+  } else if (firstElement.compareDocumentPosition(secondElement) === 2 || firstElement.compareDocumentPosition(secondElement) === 10) {
+    a(secondElement, firstElement);
   }
 };
 
-const multiplyHighlight = (secondElement, firstElement = lastSelected) => {
-  if (checkOrder(firstElement, secondElement)){
-    do {
-      if (firstElement.nextElementSibling) {
-        firstElement = firstElement.nextElementSibling;
-        firstElement.style.color = 'red';
-      } else {
-        firstElement = firstElement.parentElement;
-      }
-    } while (secondElement !== firstElement);
-  } else {
-    let temp = firstElement;
-    firstElement = secondElement;
-    secondElement = temp;
-    do {
-      if (secondElement.previousElementSibling) {
-        secondElement = secondElement.previousElementSibling;
-        secondElement.style.color = 'red';
-      } else {
-        secondElement = secondElement.parentElement;
-      }
-    } while (secondElement !== firstElement);
-  }
-
-};
-
-window.onload = ()=>{
-  let elements = folders.getElementsByTagName('div');
-  elements = Array.prototype.slice.call(elements);
-  elements.forEach((item)=>{
-    if(item.getElementsByTagName('div').length){
-      let img = document.createElement('img');
-      img.setAttribute('src', 'assets/images/triangle-icon.svg');
-      img.className = 'icon';
-      item.insertBefore(img, item.children[0]);
-    }
-  })
-}
-
-folders.addEventListener('click', (event) => {
-  let target = event.target;
-  if (target.tagName === 'SPAN' && event.ctrlKey) {
-    highlight(target.parentElement);
-  }
-  else if(target.tagName === 'SPAN' && event.shiftKey) {
-    multiplyHighlight(target.parentElement)
-  }
-  else if (target.tagName === 'SPAN' && target.nextElementSibling !== null){
-    let nextElement = target.nextElementSibling;
-    if(checkFolderState(nextElement.style.display)){
-      changeState('none', nextElement, 0);
-    } else {
-      changeState('block', nextElement, 90);
+const checkClick = (event) => {
+  const target = event.target.closest('.items-container');
+  if (target) {
+    if (target.tagName === 'SPAN' && event.ctrlKey) {
+      highlight(target);
+    } else if (target.tagName === 'SPAN' && event.shiftKey && lastSelected !== '') {
+      multiplyHighlight2(target.parentElement);
+    } else if (!!target && target.tagName === 'SPAN' && target.nextElementSibling !== null) {
+      const nextElement = target.nextElementSibling;
+      changeState(nextElement);
     }
   } else {
     unHighlightAll();
   }
-});
+};
+
+window.onload = () => {
+  let elements = folders.getElementsByTagName('div');
+  elements = Array.prototype.slice.call(elements);
+  elements.forEach((item) => {
+    if (item.getElementsByTagName('div').length) {
+      const img = document.createElement('img');
+      img.setAttribute('src', 'assets/images/triangle-icon.svg');
+      img.className = 'icon';
+      console.log(item.children[0].children[0]);
+      item = item.children[0];
+      item.insertBefore(img, item.children[0]);
+    }
+  });
+};
+
+folders.addEventListener('click', checkClick);
